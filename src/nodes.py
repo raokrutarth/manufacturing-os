156a9213fe5d530967c72bb9d79270fd7a1911d5
Nishant Rai <nishantrai18@rediff.com>
2020-04-29 03:57:27 -0700

    Add utility classes
    Adds utility classes to describe the overall system. We add items, item
    dependencies, cluster, cluster blueprints.

---
+++ src/nodes.py
@@ -0,0 +1,78 @@
+"""
+- Base Case Blueprint class to represent all nodes and their types in the graph
+    - Node:
+        - Represents details for each node
+        - Contains the item id that it produces, along with quantity (float/int)
+        - Contains the input items required to produce that quantity
+    - Blueprint:
+        - Set of all nodes
+        - Consists of some operations which need to be done by each node eventually
+    - Execution flags:
+        - Store the information of which algorithms to use
+        - Dictionary - easiest to implement for now
+    - Standard factory methods:
+        - Provide templates for common base cases
+"""
+
+from collections import namedtuple
+from typing import List
+
+
+# Structure for item: Each item has a name and id
+Item = namedtuple('Item', ['name', 'id'])
+# Item requirement - Item, Quantity
+ItemReq = namedtuple('ItemReq', ['item', 'quantity'])
+
+
+class ItemDependency(object):
+
+    def __init__(self, input_item_reqs: List[ItemReq], result_item_req: ItemReq):
+        """
+            input_item_reqs: List of input items required (Can be empty for source node)
+            result_item_req: End item produced
+        """
+        self.result_item_req = result_item_req
+        self.input_item_reqs = input_item_reqs
+
+
+class BaseNode(object):
+
+    def __init__(self, args):
+        self.node_id = args["node_id"]
+
+
+class SingleItemNode(BaseNode):
+    """
+    Implementation of single item node. Each node can produce only one type of item.
+        - Contains the item id that it produces, along with quantity (float/int)
+        - Contains the input items required to produce that quantity
+    """
+
+    def __init__(self, args):
+        super(SingleItemNode, self).__init__(args)
+        self.dependency = args["dependency"]
+
+
+class ClusterBlueprint(object):
+    """
+    Represents the cluster blueprint to create Clusters from
+    Useful for unit tests and designing test cases we want to operate on.
+    """
+
+    def __init__(self, nodes: List[BaseNode]):
+        # Set of nodes to be used by this cluster
+        self.nodes = nodes
+        # Stores any node specific operations we want to perform during execution e.g.
+        # injecting node specific random failures, delays, etc
+        self.node_specific_ops = {}
+        # Stores the flags and options we'd be using finally in our setup
+        self.flags = {}
+
+
+class Cluster(object):
+    """
+    Represents the set of nodes interacting
+    """
+
+    def __init__(self, blueprint):
+        self.nodes = blueprint.nodes()
