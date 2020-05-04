b6438a2ea0db8edde89c9274a7bdb6e5e37d36c9
Krutarth Rao <krutarth.rao@hpe.com>
2020-05-04 13:22:57 -0700

    clean up main

--- src/main.py
+++ src/main.py
@@ -1,63 +1,20 @@
-from random import randint
-import numpy as np
-
-import time
-import zmq
-import sys
-from  multiprocessing import Process
-import threading
-
-## Initial CODE with zmq
-def server(port="5556"):
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:%s" % port)
-    print("Running server on port: ", port)
-    # serves only 5 request and dies
-    for reqnum in range(5):
-        # Wait for next request from client
-        message = socket.recv()
-        print("Received request #%s: %s" % (reqnum, message))
-
-        # Send Object!!!
-        socket.send_string("World from %s" % port)
-
-def client(ports=["5556"]):
-    context = zmq.Context()
-    print("Connecting to server with ports %s" % ports)
-    socket = context.socket(zmq.REQ)
-    for port in ports:
-        socket.connect("tcp://localhost:%s" % port)
-    for request in range (20):
-        print("Sending request ", request,"...")
-        socket.send_string("Hello")
-        message = socket.recv()
-        print("Received reply ", request, "[", message, "]")
-        time.sleep (0.01)
-
-
-
-def start_process(process_id, init_inventory):
-    print(process_id, " process started with inventory ", init_inventory)
-    '''
-        TODO
-        Add new process start logic here
-    '''
-
-    while True:
-
-        sleep(30)
-
+import basecases
+import nodes
+from multiprocessing import Process
 
 def main():
-    server_ports = range(5550, 5558, 2)
-    for server_port in server_ports:
-        Process(target=server, args=(server_port,)).start()
+    demo_blueprint = basecases.dummyBlueprintCase1()
+    demo_cluster = nodes.Cluster(demo_blueprint)
+    print(demo_cluster)
 
-    # Now we can connect a client to all these servers
-    Process(target=client, args=(server_ports,)).start()
+    flags = {'runOps': True}
+    for node in demo_cluster.nodes:
+        Process(
+            target=nodes.SocketBasedNodeProcess,
+            args=(node, demo_cluster, flags),
+        ).start()
 
-    return None
+
 
 if __name__ == "__main__":
     main()
