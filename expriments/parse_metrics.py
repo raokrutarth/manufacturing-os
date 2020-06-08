3e2c5a653a68f103b9ee4b2404f281a7b4e36aba
krutarth-rao <krutarth.rao@hpe.com>
2020-06-07 21:27:41 -0700

    trivial

--- expriments/parse_metrics.py
+++ expriments/parse_metrics.py
@@ -6,19 +6,33 @@ from typing import List
 import logging
 import glob
 
-log = logging.basicConfig(
+logging.basicConfig(
     stream=sys.stdout,
-    level=logging.DEBUG
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(message)s',
+    datefmt='%H:%M:%S',
 )
+
+log = logging.getLogger()
 
 
 class Metricparser:
 
     def __init__(self, metrics_dir):
-        log.info()
+        log.info("Starting to parse exprimental metrics from {}".format(metrics_dir))
         self.metrics_files = glob.glob(metrics_dir + "/*metrics*.csv")
+        self.df = self._get_full_df()
 
-    def _get_full_df():
-        all_metrics_files =
-        return pd.concat([pd.read_csv(f) for f in all_metrics_files], ignore_index=True)
+    def _get_full_df(self):
+        return pd.concat([pd.read_csv(f) for f in self.metrics_files], ignore_index=True)
 
+    def _print_all_data(self):
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+            log.info("Full Exprimental results:\n\n%s\n\n===\n", self.df.to_string())
+
+
+
+
+if __name__ == "__main__":
+    mp = Metricparser("../tmp")
+    mp._print_all_data()
